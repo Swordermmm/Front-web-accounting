9815c4959dce15d24f698a1934beea1f84895161
Swordermmm <alyosha1leo@gmail.com>
2026-05-25 17:51:34 +0500

    Hope

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -26,6 +26,33 @@ const Login: FC<Props> = () => {
     password: "",
   };
 
+  async function login(email: string, password: string) {
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch(
+        "http://95.163.222.188:4999/api/Auth/login",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          credentials: "include",
+          headers: {
+            accept: "*/*",
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      console.log(error);
+      return "";
+    }
+  }
+
   const validationSchema = Yup.object().shape({
     email: Yup.string().required("Данное поле не заполнено!"),
     password: Yup.string().required("Данное поле не заполнено!"),
@@ -37,12 +64,11 @@ const Login: FC<Props> = () => {
     setMessage("");
 
     login(email, password)
-      .then(() => {
-        navigate("/projects");
+      .then((res) => {
+        console.log(res);
+        if (res.ok) navigate("/projects");
       })
       .catch();
-
-    console.log("ready");
   };
 
   return (
